a19d685d24cdd44616760b0baa7d0023407bf4ac
shrey27 <shreypandey27@gmail.com>
2022-09-09 14:53:37 +0530

    test cases updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const [images,setImages] = useState([]);
+  
+  async function getImages(){
+    let response = await axios.get('https://images-api.nasa.gov/search?q=moon');
+    let items = response.data.collection.items
+    let temp = []
+    
+    for(let item of items){
+      if(item && item.links){
+        let links = item.links;
+        links.forEach(linkObj => {
+          if(linkObj.href.endsWith('.jpg')){
+            temp.push(linkObj.href);
+          }
+        })
+      }
+    }
+    setImages(temp);
+  }
+
+  useEffect(() => {
+    getImages()
+  },[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="images">
+        {images.map(item => {
+          return <img key={item.slice(0,20)} src={item} className='image' alt='moon'/>
+        })}
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,28 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import axios from 'axios';
+import { act } from 'react-dom/test-utils';
+const URL = 'https://images-assets.nasa.gov/image/PIA12235/PIA12235~thumb.jpg'
+const sampleData = {
+  collection : {
+    items: [{
+      links : [{
+        href : URL
+      }]
+    }]
+  }
+}
 
-test('renders learn react link', () => {
+test('renders learn react link', async () => {
+  let spy = jest.spyOn(axios, 'get').mockResolvedValueOnce({ data: sampleData })
+  const getImages = jest.fn();
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  await act(() => {
+    getImages()
+  })
+  expect(spy).toBeCalledWith('https://images-api.nasa.gov/search?q=moon')
+  expect(getImages).toBeCalledWith()
+  const linkElement = screen.getByAltText('moon')
+  await expect(linkElement).toBeInTheDocument();
+  expect(linkElement.src).toContain(URL);
 });
